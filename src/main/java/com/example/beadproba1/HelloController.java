f124dec214d4751d4cbea8a783ffd0a08fb944bc
Ádám Zsombor <adamgombi@gmail.com>
2024-01-21 19:31:48 +0100

    hibaüzenet megjelenítése ha nem számot ír be a felhasználó

--- src/main/java/com/example/beadproba1/HelloController.java
+++ src/main/java/com/example/beadproba1/HelloController.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 
 import java.util.Random;
@@ -23,30 +22,37 @@ public class HelloController {
     private int keresettszam = rnd.nextInt(50)+1;
     @FXML
     private Label tippeltszamlabel;
-    int tippeltszam;
+    int elozolegTippeltSzam;
 
     @FXML
     protected void szamteszt(){
-
-
-        if (keresettszam > Integer.parseInt(nameField.getText()))
+        int tippeltSzam;
+        try
+        {
+            tippeltSzam = Integer.parseInt(nameField.getText());
+        }catch (Exception e)
+        {
+            massageLabel.setText("Adj meg érvényes számot!");
+            return;
+        }
+        if (keresettszam > tippeltSzam)
         {
             massageLabel.setText("^Nagyobb a keresett szám!^");
-        } else if (keresettszam < Integer.parseInt(nameField.getText())) {
+        } else if (keresettszam < tippeltSzam) {
             massageLabel.setText("ˇKissebb a keresett szám!ˇ");
         } else {
             massageLabel.setText("Eltaláltad a számot!");
             massageLabel.setTextFill(Color.GREEN);
             tippeltszamlabel.setVisible(false);
-            tippeltszam = 0;
+            tippeltSzam = 0;
             restart.setVisible(true);
         }
 
-        tippeltszamlabel.setText("Előző tippelt számod: " + tippeltszam);
-        if (tippeltszam == 0){
-            tippeltszam = Integer.parseInt(nameField.getText());
+        tippeltszamlabel.setText("Előző tippelt számod: " + elozolegTippeltSzam);
+        if (tippeltSzam == 0){
+            elozolegTippeltSzam = tippeltSzam;
         } else {
-            tippeltszam = Integer.parseInt(nameField.getText());
+            elozolegTippeltSzam = tippeltSzam;
             tippeltszamlabel.setVisible(true);
         }
     }
